be58fe177d3778499ac5e1b3bb392905e4017b27
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-08-24 21:34:10 -0400

    Follow Along Completed

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,25 @@
 // Here goes the schema for the form
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+    username: yup
+    .string()
+    .trim()
+    .required('Username Required')
+    .min(3, 'Username must be 3 Characters'),
+    email: yup
+    .string()
+    .email('must be a valid email')
+    .required('must have a valid email'),
+    role: yup
+    .string()
+    .oneOf(['instructor','student','alumni'], 'Role is required'),
+    civil: yup
+    .string()
+    .oneOf(['married', 'single'],' Status is Required'),
+    coding: yup.boolean(),
+    reading: yup.boolean(),
+    hiking: yup.boolean()
+})
+
+export default formSchema
